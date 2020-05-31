5e64adce32da7a067d778625127ca0655de0d817
Benjamin Saulon <schooldocumentsandstuff@gmail.com>
2020-05-31 07:55:59 -0400

    BULK: fix domain bug, attempt at config bug bix

--- src/ethaniccc/CrossOnlineCount/Main.php
+++ src/ethaniccc/CrossOnlineCount/Main.php
@@ -30,18 +30,31 @@ class Main extends PluginBase implements Listener{
     private static $instance;
 
     public function onEnable(){
+
+        /* The main goal right now is to actually
+        implement the config correctly. */
+
+        @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+
+        $updateTicks = (int) $this->getConfig()->get("update_ticks");
+        if(!is_integer($updateTicks)){
+            $this->getLogger()->notice("The amount of update ticks is not a whole number and therefore has defaulted to updating every 100 ticks (5 seconds)");
+            $updateTicks = 100;
+        }
         self::$instance = $this;
-        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function(int $currentTick) : void{
+        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function(int $currentTick) use($updateTicks) : void{
                $slapper = $this->getServer()->getPluginManager()->getPlugin("Slapper");
                if($slapper === null){
-                   $this->getServer()->getAsyncPool()->submitTask(new InstallSlapper());
                    $this->getLogger()->notice("The Slapper plugin is not installed, we are installing it for you.");
                    $this->getLogger()->notice("After the plugin is installed, your server will shutdown - please turn it on again.");
-               } else {
-                   /* Update server details every 0.25 seconds. */
-                   $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(int $currentTick) : void{
+                   $this->getServer()->getAsyncPool()->submitTask(new InstallSlapper());
+                } else {
+                   /* Update server details every 1 second. */
+                   /* TODO: Make this configurable in the config! */
+                   $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(int $currentTick) use($updateTicks) : void{
                        $this->updateSlapper();
-                   }), 5);
+                   }), $updateTicks);
                    $this->getServer()->getPluginManager()->registerEvents($this, $this);
                }
         }), 100);
@@ -60,90 +73,63 @@ class Main extends PluginBase implements Listener{
 		  }
     }
 
-    public static function getMain() {
+    public static function getMain() : Main{
         return self::$instance;
     }
 
-    public function updateSlapper(){
+    public function updateSlapper() : void{
       foreach($this->getServer()->getLevels() as $level) {
 			  foreach($level->getEntities() as $entity) {
 				  if(!empty($entity->namedtag->getString("server", ""))) {
                     $server = explode(":", $entity->namedtag->getString("server", ""));
+                    var_dump($server);
                     if(isset($server[0])){
-                        switch($server[0]){
-                            case "server":
-                                if(!isset($server[1])) $ip = 0;
+                        if($server[0] === "server"){
+                            if(empty($server[1])) $ip = "not_a_valid_ip";
+                            else{
+                                if(!$this->isValidIP($server[1]) && $this->is_valid_domain_name($server[1])) $ip = "not_a_valid_ip";
                                 else $ip = $server[1];
-                                if(!isset($server[2])) $port = 19132;
+                            }
+                            if(empty($server[2])) $port = 0;
+                            else{
+                                if($server[2] < 1 || $server[2] > 65536) $port = "invalid_port";
                                 else $port = $server[2];
-                                if($ip == 0) $do = false;
-                                else $do = true;
-                                if($do === true) $this->getServer()->getAsyncPool()->submitTask(new QueryServer($ip, $port, $entity->getId(), $this->getConfig()->get("server_online_message"), $this->getConfig()->get("server_offline_message")));
-                            break;
-                            case "world":
-                                if(!isset($server[1])) $world = "aa46b8ednonono";
-                                else $world = $this->getServer()->getLevelByName($server[1]);
-                                if($world === "aa46b8ednonono" || $world === null) $do = false;
-                                else $do = true;
-                                if($do === true){
-                                    $lines = explode("\n", $entity->getNameTag());
-                                    $base = $this->getConfig()->get("players_world_message");
-                                    $message = str_replace("{playing}", count($world->getPlayers()), $base);
-                                    $lines[1] = $message;
-                                    $nametag = implode("\n", $lines);
-			                        $entity->setNameTag($nametag);
-                                } else {
-                                    $lines = explode("\n", $entity->getNameTag());
-                                    $message = $this->getConfig()->get("world_error_message");
-                                    $lines[1] = $message;
-                                    $nametag = implode("\n", $lines);
-			                        $entity->setNameTag($nametag);
-                                }
-                            break;
-                            default:
-
-                            break;
+                            }
+                            if($ip !== "not_a_valid_ip" && $port !== "invalid_port") $this->getServer()->getAsyncPool()->submitTask(new QueryServer($ip, $port, $entity->getId(), $this->getConfig()->get("server_online_message"), $this->getConfig()->get("server_offline_message")));
+                        } elseif($server[0] === "world"){
+                            if(empty($server[1])) $world = "this_is_an_invalid_world";
+                            else $world = $server[1];
+                            if($world === null) $execute = false;
+                            else $execute = true;
+                            if($execute){
+                                $lines = explode("\n", $entity->getNameTag());
+                                $base = $this->getConfig()->get("players_world_message");
+                                $message = str_replace("{playing}", count($world->getPlayers()), $base);
+                                $lines[1] = $message;
+                                $nametag = implode("\n", $lines);
+			                    $entity->setNameTag($nametag);
+                            } else {
+                                $lines = explode("\n", $entity->getNameTag());
+                                $message = $this->getConfig()->get("world_error_message");
+                                $lines[1] = $message;
+                                $nametag = implode("\n", $lines);
+			                    $entity->setNameTag($nametag);
+                            }
                         }
                     }
-				  }
-			  }
-		  }
+				}
+			}
+		}
     }
 
-    public function onHitSlapper(EntityDamageByEntityEvent $event){
-        $entity = $event->getEntity();
-        $player = $event->getDamager();
-        if($entity instanceof SlapperEntity){
-
-            $nametag = explode("\n", $entity->getNameTag());
-
-            if(!isset($nametag[1])) return;
-
-            if($nametag[1] == $this->getConfig()->get("server_offline_message")){
-                if($this->getConfig()->get("server_offline_knockback") == true){
-                    $motion = $player->subtract($entity);
-                    $player->knockBack($entity, 0, $motion->x, $moition->z, $this->getConfig()->get("server_offline_knockback_amount"));
-                }
-            } 
-            
-            if($nametag[1] == $this->getConfig()->get("world_error_message")){
-                if($this->getConfig()->get("world_nofind_knockback") == true){
-                    $motion = $player->subtract($entity);
-                    $player->knockBack($entity, 0, $motion->x, $moition->z, $this->getConfig()->get("world_nofind_knockback_amount"));
-                }
-            }
-
-        }
-    }
-
-    public function onSlapperCreate(SlapperCreationEvent $ev) {
+    public function onSlapperCreate(SlapperCreationEvent $ev) : void{
 	  	$entity = $ev->getEntity();
         $lines = explode("\n", $entity->getNameTag());
         if(isset($lines[1])) $entity->namedtag->setString("server", $lines[1]);
 		$this->updateSlapper();
     }
     
-    public function onSlapperDelete(SlapperDeletionEvent $ev) {
+    public function onSlapperDelete(SlapperDeletionEvent $ev) : void{
 		  $entity = $ev->getEntity();
 		  if(!empty($entity->namedtag->getString("server", ""))) {
 			  $entity->namedtag->removeTag("server");
